abbee567a5d632e7bf9405cdbf78966ad6e7b53a
thorcamara <thorcamaraalthoff@gmail.com>
2023-08-08 20:58:51 -0300

    Added functions handleMath, flushOperation and handleNumber

--- script.js
+++ script.js
@@ -43,3 +43,39 @@ function handleSymbol(symbol) {
       break;
   }
 }
+
+function handleMath(symbol) {
+  if (buffer === '0') {
+    return;
+  }
+
+  const intBuffer = parseInt(buffer);
+
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+  previousOperator = symbol;
+  buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+  if (previousOperator === '+') {
+    runningTotal += intBuffer;
+  } else if (previousOperator === '−') {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === '×') {
+    runningTotal *= intBuffer;
+  } else if (previousOperator === '÷') {
+    runningTotal /= intBuffer;
+  }
+}
+
+function handleNumber(numberString) {
+  if (buffer === "0") {
+    buffer = numberString;
+  } else {
+    buffer += numberString;
+  }
+}
